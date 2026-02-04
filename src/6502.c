451570b8b4b280bf0117faaa346df034c7b21d9f
David Banks <dave@hoglet.com>
2026-02-04 18:32:55 +0000

    Revert "6502: vary cycles-per-line between 63 and 64 using a phase accumulator (real atom is 63.695us)"
    This reverts commit 6625e1e99c6b5106247f1a327090c3690e37e9f3.

--- src/6502.c
+++ src/6502.c
@@ -759,9 +759,6 @@ void exec6502(int linenum, int cpl)
 	int oldcyc;
 	int halfline;
 
-	// Maintain an error metric in 1000th of a cycle
-	int error = 0;
-
 	for (lines = 0; lines < linenum; lines++)
 	{
 //                rpclog("Exec line %i\n",lines);
@@ -772,13 +769,7 @@ void exec6502(int linenum, int cpl)
 		for (halfline = 0; halfline < 2; halfline++) {
 			pollsound();
 			cycles += (cpl >> 1);
-			// At cps=64 us, every half line the error is 0.152us
-			error += 152;
-			if (error >= 1000)
-			{
-				cycles--;
-				error -= 1000;
-			}
+	//                badline=0;
 			while (cycles > 0)
 			{
 				oldcyc = cycles;
